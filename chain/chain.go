e9200c021b1cf078e34ef68eb13765c01f3e8160
levy <15958732798@163.com>
2021-09-22 21:24:57 +0800

    分离迭代器

--- chain/chain.go
+++ chain/chain.go
@@ -13,41 +13,6 @@ type Chain struct {
 	DB       *bolt.DB //数据库对象
 }
 
-//链迭代器
-type ChainIterator struct {
-	CurrentHash []byte
-	DB          *bolt.DB
-}
-
-//迭代器生成
-func (chain *Chain) Iterator() *ChainIterator {
-	return &ChainIterator{
-		CurrentHash: chain.LastHash,
-		DB:          chain.DB,
-	}
-
-}
-
-//遍历迭代器
-func (ci *ChainIterator) Next() *ChainIterator {
-	var nextHash []byte
-	err := ci.DB.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(persistence.BlockBucket))
-		currentBlockBytes := b.Get(ci.CurrentHash)
-
-		currentBlock := DeserializeBlock(currentBlockBytes)
-		nextHash = currentBlock.BlockPreHash
-		return nil
-	})
-	if err != nil {
-		log.Panic(err)
-	}
-	return &ChainIterator{
-		CurrentHash: nextHash,
-		DB:          ci.DB,
-	}
-}
-
 //创世区块链
 func NewBlockChain() *Chain {
 	var lastHash []byte
