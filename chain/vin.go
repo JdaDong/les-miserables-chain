4e3193ad1ca8ba619310af690ac8a0fc9c882790
levy <15958732798@163.com>
2021-10-30 12:39:20 +0800

    交易输入集成钱包地址

--- chain/vin.go
+++ chain/vin.go
@@ -1,5 +1,10 @@
 package chain
 
+import (
+	"bytes"
+	"les-miserables-chain/utils"
+)
+
 //UTXO交易输入
 type TXInput struct {
 	TxID        []byte //交易hash id
@@ -8,7 +13,13 @@ type TXInput struct {
 	PublicKey   []byte //公钥
 }
 
+////解锁交易输入
+//func (in *TXInput) UnlockInput(unlockInputAddress string) bool {
+//	return in.ScriptSig == unlockInputAddress
+//}
+
 //解锁交易输入
-func (in *TXInput) UnlockInput(unlockInputAddress string) bool {
-	return in.ScriptSig == unlockInputAddress
+func (in *TXInput) UnlockPublicKeyHash(publicKeyHash []byte) bool {
+	publicKeyRipemd160Hash := utils.GetRipemd160(in.PublicKey)
+	return bytes.Compare(publicKeyRipemd160Hash, publicKeyHash) == 0
 }
