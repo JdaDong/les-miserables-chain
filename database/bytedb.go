2b9a0de63c270f501a10adfd313bcbba8d86b4ae
levy <15958732798@163.com>
2021-11-06 11:45:03 +0800

    [新增]增加UTXO数据桶

--- database/bytedb.go
+++ database/bytedb.go
@@ -8,6 +8,9 @@ const DbFile = "database/blockchain.database"
 //数据仓库
 const BlockBucket = "blocks"
 
+//UTXO数据桶
+const UTXOBucket = "utxo"
+
 func DbExist() bool {
 	_, err := os.Stat(DbFile)
 	return err == nil || !os.IsNotExist(err)
