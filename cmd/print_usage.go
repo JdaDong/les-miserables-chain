3d1a09466d99274ff60a0315bdbaa55555f89c84
levy <15958732798@163.com>
2021-11-28 13:56:45 +0800

    [M]修改客户端说明为中文

--- cmd/print_usage.go
+++ cmd/print_usage.go
@@ -5,11 +5,12 @@ import "fmt"
 //打印帮助信息
 func (cli *CLI) printUsage() {
 	fmt.Println("Usage:")
-	fmt.Println("\tgetbalance -addr ADDRESS -- Get balance of ADDRESS")                                        //获取余额
-	fmt.Println("\tinit -address ADDRESS -- Initialize a blockchain and send genesis block reward to ADDRESS") //初始化区块链
-	fmt.Println("\tdelete -- Delete local block data")                                                         //清空本地区块数据
-	fmt.Println("\tprintchain -- Print all the blocks of the blockchain")                                      //打印区块链
-	fmt.Println("\tsend -from FROM -to TO -amount AMOUNT -- Send AMOUNT of coins from FROM address to TO")     //转账
-	fmt.Println("\tcreatewallet -- create a wallet")
-	fmt.Println("\taddresslists -- get address lists")
+	fmt.Println("\tgetbalance -addr ADDRESS -- 获取指定地址的余额")
+	fmt.Println("\tinit -address ADDRESS -- 初始化区块链数据")
+	fmt.Println("\tdelete -- 删除区块链数据")
+	fmt.Println("\tprintchain -- 打印所有区块信息")
+	fmt.Println("\tsend -from FROM -to TO -amount AMOUNT -- 转账交易")
+	fmt.Println("\tcreatewallet -- 创建钱包")
+	fmt.Println("\taddresslists -- 获取所有钱包的地址集合")
+	fmt.Println("\tstartnode -miner MINERADDR -- 启动节点网络服务")
 }
