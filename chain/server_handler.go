f83fe182f2f31951b9b2d5b243b625d96eb1f72f
levy <15958732798@163.com>
2021-12-08 14:37:28 +0800

    [C]版本消息处理器

--- chain/server_handler.go
+++ chain/server_handler.go
@@ -1,7 +1,30 @@
 package chain
 
-func handleVersion(request []byte, bc *Chain) {
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
 
+func handleVersion(request []byte, bc *Chain) {
+	var buff bytes.Buffer
+	var payload Version
+	dataBytes := request[12:]
+
+	buff.Write(dataBytes)
+	dec := gob.NewDecoder(&buff)
+	err := dec.Decode(&payload)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	highestHeight := bc.GetHighestHeight()
+	foreignerHighestHeight := payload.BestHeight
+	if highestHeight > foreignerHighestHeight {
+		sendVersion(payload.AddrFrom, bc)
+	} else if highestHeight < foreignerHighestHeight {
+
+	}
 }
 
 func handleAddr(request []byte, bc *Chain) {
